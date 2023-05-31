666c5e92622d2739b9d487d45d1106513efa2441
sfumhmd90 <sfumhmd@gmail.com>
2023-05-30 23:56:43 -0400

    First commit

---
+++ src/hw1JavaBasics/MySelff.java
@@ -0,0 +1,25 @@
+package hw1JavaBasics;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
+
+/* naming convention
+ -- Project: File -- New -- Java Project -- Name starts with Upper Case.
+-- Package: Expand the Project name and then select src file --> right click --> New --> select package (yellow 4 box)  --> Name starts with lowercase 
+-- Class: select package --> right click --> New --> Select Class (Green circular) --> Name starts with UpperCase, class name is usually nouns or noun phrases--> Select main method
+
+ */
+
+public class MySelff {
+	// Main method is the entry point of java, also a static method, the method is
+	// actually called main and it is represented by public static void
+	// main(String[] args)
+	public static void main(String[] args) {
+		System.out.println("My name is Saif mahmud");
+		System.out.println("My name is Saif mahmud");
+		System.out.println("My Address is 1305 35th street, brooklyn, new york");
+		System.out.println("My Address is 1305 35th street, brooklyn, new york");
+		
+	}
+}
---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,25 @@
+package hw2JavaBasics;
+
+import java.awt.CardLayout;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		System.out.println(
+				" \t my name is \"Saif mahmud\", i love to drive. " + " my car info is below. \n color is black, "
+						+ "model: toyota camry se," + " year: 2019, " + " wheel drive: awd");
+		System.out.println("\n\t car color: black");
+		System.out.println("\t car color: black");
+		System.out.println("\t model: camry se");
+		System.out.println("\t model: camry se");
+		System.out.println("\t year: 2019");
+		System.out.println("\t year: 2019");
+		System.out.println("\t wheel drive: awd");
+		System.out.println("\t wheel drive: awd");
+		System.out.println("\n-----------------------\n");
+		System.out.print("\t car color: black,");
+		System.out.print(" model: camry se,");
+		System.out.print(" year: 2019,");
+		System.out.print(" wheel drive: awd");
+	}
+}
