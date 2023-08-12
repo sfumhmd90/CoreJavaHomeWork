081d26bbb75bbdeaeceb5a84756710aada3edfa1
sfumhmd90 <sfumhmd@gmail.com>
2023-08-11 23:34:16 -0400

    formatted

--- src/hw7UseOfNestedIf/DiabeticCondition.java
+++ src/hw7UseOfNestedIf/DiabeticCondition.java
@@ -9,23 +9,19 @@ public class DiabeticCondition {
 
 		Scanner scanner = new Scanner(System.in);
 		double hbA1c = scanner.nextDouble();
-		if(hbA1c>6.4) 
-		{ System.out.println("I am a diabetic patient");}
-		  
-		else if (hbA1c<6.4) {
+		if (hbA1c > 6.4) {
+			System.out.println("I am a diabetic patient");
+		}
+
+		else if (hbA1c < 6.4) {
 			System.out.println("Congratulation! you are not diabetic");
-			if( hbA1c >= 5.7) {
+			if (hbA1c >= 5.7) {
 				System.out.println("I am a pre-diabetic patient");
-			}else if(hbA1c < 5.7) {
+			} else if (hbA1c < 5.7) {
 				System.out.println("I am a healthy person");
-				}
-		}else {System.out.println("enter valid number");}}}
-		
-		
-		
-		
-	    
-	
-
-
-
+			}
+		} else {
+			System.out.println("enter valid number");
+		}
+	}
+}
